ce781b76b93082dda399118f36a552822529b4d4
Nathan Kuik <nathan.kuik@gmail.com>
2019-12-04 20:18:17 +0100

    Day two solved

--- 02/test_opcode.py
+++ 02/test_opcode.py
@@ -4,13 +4,19 @@ from opcode_handler import handle_input
 
 
 def test_first_case_opcode():
-    input = "1,9,10,3,2,3,11,0,99,30,40,50"
-    expected_output = "3500,9,10,70,2,3,11,0,99,30,40,50"
-    
-    assert expected_output == handle_input(input) 
+    input = '1,9,10,3,2,3,11,0,99,30,40,50'.split(',')
+    expected_output = '3500,9,10,70,2,3,11,0,99,30,40,50'
 
-def second_case_opcode():
-    input = "1,1,1,4,99,5,6,0,99"
-    expected_output = "30,1,1,4,2,5,6,0,99"
-    
-    assert expected_output == handle_input(input) 
+    assert expected_output == handle_input(input)
+
+def test_second_case_opcode():
+    input = '1,1,1,4,99,5,6,0,99'.split(',')
+    expected_output = '30,1,1,4,2,5,6,0,99'
+
+    assert expected_output == handle_input(input)
+
+def test_third_case_opcode():
+    input = '2,4,4,5,99,0'.split(',')
+    expected_output = '2,4,4,5,99,9801'
+
+    assert expected_output == handle_input(input)
