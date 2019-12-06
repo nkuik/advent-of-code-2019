ebb1809b4f9e11fd1d48f19931eb2e906809b065
Nathan Kuik <nathan.kuik@gmail.com>
2019-12-06 22:58:13 +0100

    Day 04 part 2

--- 04/password_combinatorial.py
+++ 04/password_combinatorial.py
@@ -10,14 +10,25 @@ def is_not_decreasing(num_str: str) -> bool:
         comparisons.append(num_str[i] <= num_str[i + 1])
     return all(comparisons)
 
+
 def has_double(num_str: str) -> bool:
-    comparisons = []
-    for i in range(len(num_str) - 1):
-        comparisons.append(num_str[i] == num_str[i + 1])
-    return any(comparisons)
+    count = {}
+    for number in num_str:
+        if number in count:
+            count[number] += 1
+        else:
+            count[number] = 1
+
+    over_two = any((val > 2 for val in count.values()))
+    at_least_one = any((val == 2 for val in count.values()))
+    if over_two and not at_least_one:
+        return False
+    if at_least_one:
+        return True
+    return False
+
 
 checked_nums = {}
-
 for num in range(int(first_part), int(second_part)):
     if num not in checked_nums:
         if all((is_not_decreasing(str(num)),
@@ -25,5 +36,6 @@ for num in range(int(first_part), int(second_part)):
             checked_nums[num] = True
         else:
             checked_nums[num] = False
+
 
 print(f'{sum(value is True for value in checked_nums.values())}')
