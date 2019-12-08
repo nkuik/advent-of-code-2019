f38176ab9cf9b124e9a42c16b3de8de26da43c6b
Nathan Kuik <nathan.kuik@gmail.com>
2019-12-08 12:20:36 +0100

    Day 6 part 1

--- 06/orbit_graph.py
+++ 06/orbit_graph.py
@@ -10,14 +10,20 @@ with open('puzzle_input.csv', 'r') as csvfile:
         orbits += row
 
 
-direct_orbits = {}
+bidirectional_orbits = {}
 indirect_orbits = {}
 for orbit in orbits:
     origin = orbit.split(')')[0]
     orbiter = orbit.split(')')[1]
-    if origin not in direct_orbits:
-        direct_orbits[origin] = []
-    direct_orbits[origin].append(orbiter)
+
+    if origin not in bidirectional_orbits:
+        bidirectional_orbits[origin] = []
+    if orbiter not in bidirectional_orbits:
+        bidirectional_orbits[orbiter] = []
+
+    bidirectional_orbits[origin].append(orbiter)
+    bidirectional_orbits[orbiter].append(origin)
+
     indirect_orbits[orbiter] = origin
 
 
@@ -30,6 +36,25 @@ def find_all_orbits(orbit_dict: Dict, origin: str) -> int:
 total_orbits = 0
 for planet in indirect_orbits.keys():
     total_orbits += find_all_orbits(indirect_orbits, planet)
+
+# From: https://www.python.org/doc/essays/graphs/
+def find_shortest_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not graph.has_key(start):
+        return None
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
+
+
+shortest_path = find_shortest_path(bidirectional_orbits, 'YOU', 'SAN')
 
 
 print(f'Total orbits is {total_orbits}')
