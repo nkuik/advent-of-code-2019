51f6f41464d62e6e39357ca4114c22706bb970ff
Nathan Kuik <naku0510@dsb.dk>
2019-12-09 22:21:24 +0100

    Change name of package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='challenges',
+    name='advent_of_code',
     version='0.1.0',
-    packages=find_packages('challenges'),
+    packages=find_packages('advent_of_code'),
 )
